c5c58f4bd508e2e51dc6093a075287f105124918
Wadhah mahrouk <wadhah.mahroug15@gmail.com>
2020-11-11 10:41:50 +0100

    update icon

--- components/app/AppHead/index.tsx
+++ components/app/AppHead/index.tsx
@@ -8,7 +8,7 @@ const AppHead: React.FunctionComponent = () => {
   return (
     <Head>
       <title>GL3 time table</title>
-      <link rel="icon" type="image/webp" href="/favicon.webp" />
+      <link rel="icon" type="image/webp" href="icons/icon.svg" />
       <meta name="application-name" content={APP_NAME} />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -22,13 +22,9 @@ const AppHead: React.FunctionComponent = () => {
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
       />
 
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/icons/apple-touch-icon.png"
-      />
+      <link rel="apple-touch-icon" sizes="any" href="/icons/icon.svg" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="shortcut icon" href="/icons/favicon.ico" />
+      <link rel="shortcut icon" href="/icons/icon.svg" />
     </Head>
   )
 }
