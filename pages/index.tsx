b66532250eabb7643e6bab01c71930eed9804534
Wadhah mahrouk <wadhah.mahroug15@gmail.com>
2020-11-11 21:31:58 +0100

    data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import { NextPage } from 'next'
 import React from 'react'
 import BasePage from '../components/shared/BasePage'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { FiliereWithLevel } from '@prisma/client'
 
-export const Home: NextPage = () => {
+interface Props {
+  data: FiliereWithLevel
+}
+
+export const Home: NextPage<Props> = () => {
   return <BasePage></BasePage>
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: false,
+  }
+}
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      data: null,
+    },
+  }
 }
 
 export default Home
